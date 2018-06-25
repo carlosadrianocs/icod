00f415091e6abbdd7641c5c515aea203b2aa7270
Carlos Adriano <adryannoplay10@gmail.com>
2018-06-25 15:49:37 -0300

    Editando as Paginas

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,9 @@ var sass = require('gulp-sass');
 
 //Compilar o Sass
 gulp.task('sass', function() {
-    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
+    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'scss/*.scss'])
         .pipe(sass())
-        .pipe(gulp.dest("src/css"))
+        .pipe(gulp.dest("css"))
         .pipe(browsersync.stream());
 
 });
@@ -14,55 +14,20 @@ gulp.task('sass', function() {
 //mover js para src.js
 gulp.task('js', function() {
     return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
-        .pipe(gulp.dest("src/js"))
+        .pipe(gulp.dest("js"))
         .pipe(browsersync.stream());
 
 });
-
 
 //servidor para olhar os Html /scss
 gulp.task('server', ['sass'], function() {
     browsersync.init({
-        server: "./src"
+        server: "./"
 
     });
 
-    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
-    gulp.watch("src/*.html").on('change', browsersync.reload);
-
-});
-
-gulp.task('default', ['js', 'server']);var gulp = require('gulp');
-var browsersync = require('browser-sync').create();
-var sass = require('gulp-sass');
-
-//Compilar o Sass
-gulp.task('sass', function() {
-    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
-        .pipe(sass())
-        .pipe(gulp.dest("src/css"))
-        .pipe(browsersync.stream());
-
-});
-
-//mover js para src.js
-gulp.task('js', function() {
-    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
-        .pipe(gulp.dest("src/js"))
-        .pipe(browsersync.stream());
-
-});
-
-
-//servidor para olhar os Html /scss
-gulp.task('server', ['sass'], function() {
-    browsersync.init({
-        server: "./src"
-
-    });
-
-    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
-    gulp.watch("src/*.html").on('change', browsersync.reload);
+    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'scss/*.scss'], ['sass']);
+    gulp.watch("/*.html").on('change', browsersync.reload);
 
 });
 
